d9624f1cbf395f1a5fc47947ae2eda182f818861
qbs0001 <qbs0001@sample.com>
2020-06-03 17:33:26 +0900

    Initial Commit

---
+++ walkApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  walkApp
+//
+//  Created by 0001 QBS on 2020/06/03.
+//  Copyright © 2020 qbs0001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
